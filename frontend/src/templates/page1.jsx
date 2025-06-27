bfaa13b46232ae8f0fea37389cc4dad70f5c3c33
mico-m31 <mico.martin@student.president.ac.id>
2025-06-27 16:34:43 +0700

    updating ui in page1

--- frontend/src/templates/page1.jsx
+++ frontend/src/templates/page1.jsx
@@ -1,23 +1,32 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import { Plus } from "lucide-react";
+import Button from "@mui/material/Button";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
 
 function DataCard({ data }) {
-  return(
-  <>
-    {data.map((item, index) => (
-      <React.Fragment key={index}>
-        <div>
-          <li>{item.date}</li>
-          <li>{item.income}</li>
-          <li>{item.expense}</li>
-          <li>{item.total}</li>
-        </div>
-      </React.Fragment>
-    ))}
-  </>
-  )
+  return (
+    <>
+      {data.map((item, index) => (
+        <React.Fragment key={index}>
+          <div>
+            <li>{item.date}</li>
+            <li>{item.income}</li>
+            <li>{item.expense}</li>
+            <li>{item.total}</li>
+          </div>
+        </React.Fragment>
+      ))}
+    </>
+  );
 }
 
 export default function Page1() {
+  const [isOpen, Closed] = useState("");
+
   const date = useRef();
   const income = useRef();
   const expense = useRef();
@@ -31,7 +40,7 @@ export default function Page1() {
       income: income.current.value,
       expense: expense.current.value,
       category: category.current.value,
-      note: note.current.value
+      note: note.current.value,
     };
     console.log("Sending data:", data);
 
@@ -53,10 +62,10 @@ export default function Page1() {
   };
   return (
     <div>
-      <ul className="text-black grid grid-cols-4 p-4 text-center">
-        <li className="p-4 mt-[-5px]">Date</li>
-        <li className="p-4">Income</li>
-        <li className="p-4">Expense</li>
+      <ul className="text-black grid grid-cols-4 p-2 text-center">
+        <li className="p-4 mt-[-5px] ">Date</li>
+        <li className="p-4 text-blue">Income</li>
+        <li className="p-4 text-red">Expense</li>
         <li className="p-4">Total</li>
         <li></li>
         <li>Rp </li>
@@ -65,33 +74,65 @@ export default function Page1() {
       </ul>
       <hr></hr>
       <div>
-        <ul className="text-black grid grid-cols-4 p-4 text-center">
-        </ul>
+        <ul className="text-black grid grid-cols-4 p-4 text-center"></ul>
       </div>
-
-      <form onSubmit={handleSubmit} className="">
-        <label>
-          Date:
-          <input type="date" className="border" ref={date} ></input>
-        </label>
-        <label>
-          Income:
-          <input type="text" className="border" ref={income}></input>
-        </label>
-        <label>
-          Expense
-          <input type="text" className="border" ref={expense}></input>
-        </label>
-        <label>
-          Category
-          <input type="text" className="border" ref={category}></input>
-        </label>
-        <label>
-          Note
-          <input type="text" className="border" ref={note}></input>
-        </label>
-        <button type="submit">submit</button>
-      </form>
+      <div className="flex justify-center mt-[50px] hidden">
+        <div className="border-1 rounded-lg p-4 w-1/4 flex justify-center ">
+          <form onSubmit={handleSubmit} className="flex flex-col p-4">
+            <label className="p-2">
+              Date:
+              <input
+                type="date"
+                className="border cursor-pointer"
+                ref={date}
+              ></input>
+            </label>
+            <label className="p-2">
+              Income:
+              <TextField
+                id="standard-basic"
+                label="Standard"
+                variant="standard"
+                ref={income}
+              />
+            </label>
+            <label className="p-2">
+              Expense
+              <TextField
+                id="standard-basic"
+                label="Standard"
+                variant="standard"
+                ref={expense}
+              />
+            </label>
+            Category:
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label" ref={category}>
+                Age
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value=""
+                label="Age"
+                onChange=""
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+            <label className="p-2">
+              Note
+              <input type="text" className="border" ref={note}></input>
+            </label>
+            <Button variant="outlined">Submit</Button>
+          </form>
+        </div>
+      </div>
+      <button className="w-17 h-17 rounded-full bg-blue-500 text-white flex items-center justify-center hover:bg-blue-600 cursor-pointer fixed top-164 left-354">
+        <Plus size={34} /> 
+      </button>
     </div>
   );
 }
