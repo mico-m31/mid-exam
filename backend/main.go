26f0bfaf35c829b549a52f3dad363a1ab4e2fd14
mico-m31 <mico.martin@student.president.ac.id>
2025-06-27 10:55:17 +0700

    adding api

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,69 @@
 package main
-import(
-	
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"log"
+	_ "github.com/lib/pq"
 )
+type DailyData struct {
+	Id int `json:"id"`
+	Date string `json:"date"`
+	Income string `json:"income"`
+	Expense string `json:"expense"`
+	Category string `json:"category"`
+	Note string `json:"note"`
+}
+
+
+func dbConnection(){
+	const (
+	host = "localhost"
+	port = "5432"
+	user = "postgres"
+	password = "123"
+	dbName = "user_data"
+	)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user%s" + "password=%s dbname=%s",host,port,user,password,dbName)
+	db,err := sql.Open("postgres", psqlInfo)
+	if err != nil{
+		panic(err)
+	}
+	defer db.Close()
+}
+
+func handleUserData(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+	 if r.Method != http.MethodPost {
+                http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+                return
+        }
+
+	var data DailyData
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil{
+		http.Error(w, "error wak walawe", http.StatusBadRequest)
+		return
+	}
+
+		json.NewEncoder(w).Encode(map[string]string{
+		"message": "Data received",
+	})
+	fmt.Println("Received:", data.Date, data.Income, data.Expense, data.Category, data.Note)
+
+}
 
 func main(){
+	http.HandleFunc("/api/userData", handleUserData)
 
+	log.Fatal(http.ListenAndServe(":8080",nil))
 }
--- frontend/src/templates/page1.jsx
+++ frontend/src/templates/page1.jsx
@@ -1,14 +1,97 @@
-import React from 'react';
+import React, { useRef } from "react";
+
+function DataCard({ data }) {
+  return(
+  <>
+    {data.map((item, index) => (
+      <React.Fragment key={index}>
+        <div>
+          <li>{item.date}</li>
+          <li>{item.income}</li>
+          <li>{item.expense}</li>
+          <li>{item.total}</li>
+        </div>
+      </React.Fragment>
+    ))}
+  </>
+  )
+}
 
 export default function Page1() {
+  const date = useRef();
+  const income = useRef();
+  const expense = useRef();
+  const category = useRef();
+  const note = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      date: date.current.value,
+      income: income.current.value,
+      expense: expense.current.value,
+      category: category.current.value,
+      note: note.current.value
+    };
+    console.log("Sending data:", data);
+
+    fetch("http://localhost:8080/api/userData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log("Data sent successfully:", result);
+        alert("data submitted!");
+      })
+      .catch((err) => {
+        console.error("Error sending data:", err);
+      });
+  };
   return (
     <div>
       <ul className="text-black grid grid-cols-4 p-4 text-center">
-        <li className="p-4">Date</li>
+        <li className="p-4 mt-[-5px]">Date</li>
         <li className="p-4">Income</li>
         <li className="p-4">Expense</li>
         <li className="p-4">Total</li>
+        <li></li>
+        <li>Rp </li>
+        <li>Rp </li>
+        <li>Rp </li>
       </ul>
+      <hr></hr>
+      <div>
+        <ul className="text-black grid grid-cols-4 p-4 text-center">
+        </ul>
+      </div>
+
+      <form onSubmit={handleSubmit} className="">
+        <label>
+          Date:
+          <input type="date" className="border" ref={date} ></input>
+        </label>
+        <label>
+          Income:
+          <input type="text" className="border" ref={income}></input>
+        </label>
+        <label>
+          Expense
+          <input type="text" className="border" ref={expense}></input>
+        </label>
+        <label>
+          Category
+          <input type="text" className="border" ref={category}></input>
+        </label>
+        <label>
+          Note
+          <input type="text" className="border" ref={note}></input>
+        </label>
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
